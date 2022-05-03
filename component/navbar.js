d6171843482ee59b1db3b8436b4d7a42f995fa64
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-05-03 16:31:51 +0530

    add hambarger

--- component/navbar.js
+++ component/navbar.js
@@ -56,5 +56,57 @@ function navbar(){
 }
 
 // console.log(navbar())
+function hambarger(){
+    return`  <div id="hambarger-main-top">
+    <div id="hambarger-top">
+        <div id="hambarger-top-img">
+           <a href="index.html"><img src="https://www.hindustantimes.com/res/images/logo.png" alt=""></a>
+        </div>
+        <div id="hambarger-top-loction">
+            New Delhi 37 C <br> Tuesday,May 03,2022
+        </div>
+        <div id="hambarger-top-wrong">
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+    </div>
+    <hr style="border: 0.5px solid ;">
+    </div>
+    <div id="hambarger-bottom">
+        <div id="hamberger-option">
+            <a href="#">Home</a>
+            <a href="#">India News</a>
+            <a href="#">Entertainment</a>
+            <a href="#">IPL 2022</a>
+            <a href="#">Election</a>
+            <a href="#">Cities</a>
+            <a href="#">World News</a>
+            <a href="#">Lifestyle</a>
+            <a href="#">Trending</a>
+            <a href="#">Education</a>
+            <a href="#">Astrology</a>
+            <a href="#">Opinion</a>
+            <a href="#">Analysis</a>
+            <a href="#">Video</a>
+            <a href="#">photo</a>
+            <a href="#">Sport</a>
+            <a href="#">Web Stories</a>
+            <a href="#">Business</a>
+        </div>
+       
+        <div id="hambarger-bottom-signIn">
+            
+<div id="sign-in">
+<a href="#">Sign in</a>
+</div>
 
-export {navbar}
+<div id="social-media">
+
+<div id="social-media-facebook"><i class="fa-brands fa-facebook-square"></i></div>
+<div id="social-media-twitter"><i class="fa-brands fa-twitter-square"></i></div>
+<div id="social-media-linkedin"><i class="fa-brands fa-linkedin"></i></div>
+</div>
+        </div>
+    </div>`
+}
+
+export {navbar,hambarger}
--- script/index.js
+++ script/index.js
@@ -1,3 +1,14 @@
-import { navbar } from "../component/navbar.js";
+import { navbar ,hambarger} from "../component/navbar.js";
 
 document.getElementById("main-navbar").innerHTML=navbar();
+
+document.getElementById("hambarger").innerHTML=hambarger()
+
+document.getElementById("navbar-left-hambargur").addEventListener("click",function(){
+  let  explore=document.getElementById("hambarger")
+    explore.style.display="block";
+})
+document.getElementById("hambarger-top-wrong").addEventListener("click",function(){
+  let  explore=document.getElementById("hambarger")
+    explore.style.display="none";
+})
