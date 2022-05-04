aaeeb16f9438eabf560a99cb1c82114efd66166b
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-05-05 04:30:47 +0530

    add container css and javascript

--- script/index.js
+++ script/index.js
@@ -19,3 +19,202 @@ document.getElementById("sign-hambarger-cross").addEventListener("click",functio
     let signHambarger=document.getElementById("sign-hamberger")
     signHambarger.style.display="none"
 })
+
+
+
+
+
+
+
+// .........f?otter............
+
+
+async function search() {
+  // console.log("hii");
+  try {
+      let me = "india";
+      let URL = `https://ndtvnews-api.herokuapp.com/general?category=values(${me})&field=values(headline,posted_date,description,url,image_url)`;
+      const res = await fetch(URL);
+      // console.log(res)
+      const data = await res.json()
+
+      console.log(data.news[0].articles)
+      const Data=data.news[0].articles
+
+      appendData(Data)
+
+  } catch (err) {
+      console.log(err)
+  }
+}
+
+search()
+
+function appendData(data){
+
+  data.forEach(el => {
+      const box=document.createElement("div")
+      const indiaNews=document.createElement("h2")
+      indiaNews.innerText="India News"
+      indiaNews.style.color="#00b1cd"
+
+      const headline=document.createElement("h3")
+      headline.innerText=el.headline
+      headline.setAttribute("class","indiaNews-headline")
+
+      const posted_date=document.createElement("p")
+      posted_date.innerText=`Updated ${el.posted_date}`
+
+      box.append(indiaNews,headline,posted_date)
+
+      document.getElementById("hindustanNews-letest-top").append(box)
+
+
+
+
+
+  });
+}
+
+async function mainNews() {
+  // console.log("hii");
+  try {
+      let me = "latest";
+      let URL = `https://ndtvnews-api.herokuapp.com/general?category=values(${me})&field=values(headline,posted_date,description,url,image_url)`;
+      const res = await fetch(URL);
+      // console.log(res)
+      const data = await res.json()
+
+      console.log(data.news[0].articles)
+      const Data=data.news[0].articles
+
+      appendmainNewsData(Data)
+
+  } catch (err) {
+      console.log(err)
+  }
+}
+
+mainNews()
+
+function appendmainNewsData(data){
+
+  data.forEach((el)=>{
+  
+      const mainDav=document.createElement("div")
+      mainDav.setAttribute("id","mainDav")
+      const mainDavleft=document.createElement("div")
+ 
+      const mainDavimg=document.createElement("div")
+      let image_url=document.createElement("img")
+      image_url.src=el.image_url
+      image_url.setAttribute("id","news-img")
+      mainDavimg.append(image_url)
+      const mainDavleftbot=document.createElement("div")
+      mainDavleftbot.setAttribute("id","mainDavleftbot")
+      const mainDavleftbotleft=document.createElement("div")
+      const mainDavleftbotright=document.createElement("div")
+      mainDavleftbotright.setAttribute("id","mainDavleftbotright")
+      let div=document.createElement("div")
+      let itag=document.createElement("i")
+      itag.setAttribute("class","fa-regular fa-bookmark")
+      let itag1=document.createElement("i")
+      itag1.setAttribute("class","fa-solid fa-share-nodes")
+      div.append(itag)
+      // div.innerHTML=<i class="fa-regular fa-bookmark"></i>
+      let count=0
+      div.addEventListener("click",function(){
+          count++
+          if(count%2==1){
+          itag.setAttribute("class","fa-solid fa-bookmark")
+          savedData(el)
+          }else{
+              itag.setAttribute("class","fa-regular fa-bookmark")
+          }
+      })
+      let div1=document.createElement("div")
+      // div1.innerHTML=<i class="fa-solid fa-share-nodes"></i>
+      div1.append(itag1)
+      const indiaNews=document.createElement("h2")
+      indiaNews.innerText="India News"
+      indiaNews.style.color="#00b1cd"
+
+      const headline=document.createElement("h3")
+      headline.innerText=el.headline
+    
+      
+
+      const posted_date=document.createElement("p")
+      posted_date.innerText=`Updated ${el.posted_date}`
+      mainDavleftbotright.append(div1,div)
+      mainDavleftbot.append(mainDavleftbotleft,mainDavleftbotright)
+      mainDavleftbotleft.append(posted_date)
+     
+      mainDavleft.append(indiaNews,headline,mainDavleftbot)
+      mainDav.append(mainDavleft,mainDavimg)
+
+      document.getElementById("hidustanNews-mainNews-box").append(mainDav)
+
+  })
+}
+
+function savedData(el){
+  arr=[]
+  arr.push(el)
+  localStorage.setItem("saved",JSON.stringify(arr))
+}
+
+
+async function newsads() {
+  // console.log("hii");
+  try {
+      let me = "football";
+      let URL = `https://ndtvnews-api.herokuapp.com/sports?sport=values(${me})&field=values(headline,posted_date,description,url,image_url)`;
+      const res = await fetch(URL);
+      // console.log(res)
+      const data = await res.json()
+
+      console.log(data.news[0].articles)
+      const Data=data.news[0].articles
+
+      appendnewsadsData(Data)
+
+  } catch (err) {
+      console.log(err)
+  }
+}
+
+newsads()
+
+function appendnewsadsData(data){
+
+  data.forEach(el => {
+      const box=document.createElement("div")
+      const indiaNews=document.createElement("h2")
+      indiaNews.innerText="India News"
+      indiaNews.style.color="#00b1cd"
+
+      const headline=document.createElement("h3")
+      headline.innerText=el.headline
+      headline.setAttribute("class","indiaNews-headline")
+
+      const image_url=document.createElement("div")
+      const imgurl=document.createElement("img")
+      imgurl.src=el.image_url
+      imgurl.setAttribute("id","vedi")
+      image_url.append(imgurl)
+      // console.log(el.url)
+
+      const posted_date=document.createElement("p")
+      posted_date.innerText=`Updated ${el.posted_date}`
+
+      box.append(indiaNews,headline,posted_date,image_url)
+
+      document.getElementById("hindustanNews-ads-box").append(box)
+
+
+
+
+
+  });
+}
